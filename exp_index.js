44987dade1fb9a2788273205aad2d914b28d57ca
saketh-r0ck <saisaketh5022@gmail.com>
2023-03-22 00:54:19 +0530

    version -1.2 signup databased added

--- exp_index.js
+++ exp_index.js
@@ -1,9 +1,19 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
-
+const mongoose = require('mongoose');
 const app = express();
 const port = 3000;
+
+
+mongoose.connect('mongodb://localhost:27017/careersdb',{useNewUrlParser: true})
+    .then(() => {
+        console.log("MOngo Db connection open!!");
+    })
+    .catch((err) =>{
+        console.log("Oh no Mongo Connection Error!!!!");
+        console.log(err);
+    });
 
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static(__dirname+"/public"));
@@ -21,11 +31,11 @@ app.get('/',function(req,res){
 });
 
 app.get('/login.html',(req,res)=>{
-    res.sendFile(__dirname+'/login.html');
+    res.render('/login',{Message:""});
 });
 
 app.get('/signup.html',function(req,res){  
-    res.sendFile(__dirname+"/signup.html");
+    res.render("/signup",{Message : ""});
 });
 
 app.get('/userjobslist',(req,res)=>{
@@ -43,10 +53,7 @@ app.post("/login",function(req,res){
     res.render('userpage',{user:loggedUser});
 });
 
-app.post("/signup",function(req,res){
-    userslist.push(req.body);
-    res.sendFile(__dirname+"/login.html");
-});
+
 
 /*      admin     */
 
@@ -59,13 +66,57 @@ app.post("/admin",function(req,res){
 app.get("/adminIndex.html",(req,res)=>{
     res.render('adminIndex');
 });
-app.get("/usersList.html",(req,res)=>{
-    
-    res.render('usersList',{ulists:userslist});
-});
+
 app.get("/applications.html",(req,res)=>{
     res.render('applications');
 });
 app.get("/manageJobs.html",(req,res)=>{
     res.render('manageJobs');
+});
+
+
+
+/*  users list  */
+
+    /* New User - Signup */
+
+app.post("/signup",function(req,res){
+     
+    const newuser = new User({
+        userName: req.body.username,
+        firstName: req.body.firstname,
+        lastName: req.body.secondname,
+        email: req.body.email,
+        password: req.body.password
+    });
+    User.find({userName : req.body.username }).then(function(foundUsersList){
+        if(foundUsersList.length === 0){
+            console.log('User Added Successfully');
+            newuser.save();
+            res.render("/login",{Message:"User Added Successfully"});
+        }else{
+            console.log("User Existed try another user");
+            res.redirect("/signup.html");
+        }
+    });
+    
+    
+});
+
+const usersSchema = new mongoose.Schema({
+    userName : String,
+    firstName : String,
+    lastName : String,
+    email : String,
+    password : String
+});
+
+const User = mongoose.model('User',usersSchema);
+
+    /*   admin users list  */ 
+
+app.get("/usersList.html",(req,res)=>{
+    User.find().then(function(foundUsersList){
+        res.render('usersList',{ulists : foundUsersList});
+    });
 });
